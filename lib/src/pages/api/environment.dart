1be3a446a01fccae644f31fc1f9170773127da03
Zedrok <42598697+Zedrok@users.noreply.github.com>
2023-04-08 03:56:22 -0400

    Eliminación de API key por publicación de repo

--- lib/src/pages/api/environment.dart
+++ lib/src/pages/api/environment.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: constant_identifier_names
 
 class Environment {
-  static const String API_KEY_MAPS = 'AIzaSyCZWR9O1Nb7U0CcXVJqwAGqOU8CjaQX4h0';
+  static const String API_KEY_MAPS = 'API_KEY';
 }
